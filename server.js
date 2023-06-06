8d8a7e42a079ea5c7302cda7e6b29891f8b5ae93
OleksiiKostenko <kostenko.oleksii91@gmail.com>
2023-06-06 16:14:33 +0300

    create .env file

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 const app = require('./app');
-const DB_HOST =
-  'mongodb+srv://Oleksii:I7BE3XIXZ8TlkdGF@cluster0.jvzsfsv.mongodb.net/db-contacts?retryWrites=true&w=majority';
+require('dotenv').config();
+const { DB_HOST, PORT } = process.env;
 
 const { connect } = require('mongoose');
 connect(DB_HOST)
   .then(() => {
-    app.listen(3000, () => {
-      console.log('Server running. Use our API on port: 3000');
+    app.listen(PORT, () => {
+      console.log('Database connection successful');
     });
   })
   .catch(error => {
     console.log(error);
     process.exit(1);
   });
-
-// I7BE3XIXZ8TlkdGF
