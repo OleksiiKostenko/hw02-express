b64a0c1065eebdb39ed00caa92fb3051499d508b
OleksiiKostenko <kostenko.oleksii91@gmail.com>
2023-06-06 15:48:05 +0300

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const app = require('./app');
+const DB_HOST =
+  'mongodb+srv://Oleksii:I7BE3XIXZ8TlkdGF@cluster0.jvzsfsv.mongodb.net/db-contacts?retryWrites=true&w=majority';
 
-app.listen(3000, () => {
-  console.log('Server running. Use our API on port: 3000');
-});
+const { connect } = require('mongoose');
+connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log('Server running. Use our API on port: 3000');
+    });
+  })
+  .catch(error => {
+    console.log(error);
+    process.exit(1);
+  });
+
+// I7BE3XIXZ8TlkdGF
